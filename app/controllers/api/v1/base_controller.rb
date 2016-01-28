fdf52a9f2e56ee61cb4afac51d183c96a708e888
yuanjianhe <jianhe.yuan@weizhuli.com>
2016-01-28 19:39:47 +0800

    add api_error back status

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,4 +3,7 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
   before_action :destroy_session
   skip_before_action :verify_authenticity_token
+  def api_error(opts = {})
+    render nothing: true, status: opts[:status]
+  end
 end
